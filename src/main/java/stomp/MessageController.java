6e887a09b731ca6cc334f8800ec550528c425b17
황주원 <juwon8891@gmail.com>
2022-07-08 00:48:57 +0900

    feat: stomp 소켓 통신 구현

--- src/main/java/stomp/MessageController.java
+++ src/main/java/stomp/MessageController.java
@@ -1,6 +1,8 @@
 package stomp;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -9,14 +11,17 @@ import somaMVP.response.ImageResponse;
 
 @Profile("stomp")
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 public class MessageController {
 
     private final SimpMessageSendingOperations sendingOperations;
 
     @MessageMapping("/upload")
-    public void enter(ImageResponse imageResponse) {
-        imageResponse.sequenceNo++;
-        sendingOperations.convertAndSend("/upload",imageResponse.getSequenceNo());
+    public void enter(Object obj) {
+        log.info("request = {}", obj);
+        //imageResponse.sequenceNo++;
+        //sendingOperations.convertAndSend("/upload",imageResponse.getSequenceNo());
+        sendingOperations.convertAndSend("/upload",obj);
     }
 }
--- src/main/java/stomp/StompWebSocketConfig.java
+++ src/main/java/stomp/StompWebSocketConfig.java
@@ -14,7 +14,7 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/upload").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/upload").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
